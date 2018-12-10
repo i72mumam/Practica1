58bd760dea169d47e3664f443d440c52104307d5
Ángel Villegas del Río <angelvillegas7@gmail.com>
2018-12-10 20:01:09 +0100

    Añadir borrar alumno

--- Practica4/Código/Alumno.cc
+++ Practica4/Código/Alumno.cc
@@ -1,7 +1,10 @@
 
 #include "Alumno.h"
 
-bool Agenda::ExisteAlumno(string DNI){
+using namespace std;
+
+
+bool Alumno::InsertarAlumno(string DNI){
 ListaAlumnos Alumnos;
 string linea;
   ifstream entrada;
@@ -16,3 +19,20 @@ string linea;
         cout<<"El alumno se guarda en la base de datos"<<endl;
   return true;
 }
+}
+void Alumno::borraralumno(string DNI){
+  ListaAlumnos Alumnos;
+  string linea;
+   ifstream entrada;
+   entrada.open("Alumnos.bin", ios::in| ios:binary);
+     if(entrada.is_open()){
+       while(getline(entrada,linea, ' ')){
+           if(Alumnos.DNI_==DNI){
+               setAlumno(NULL);
+           }
+       }
+       else{
+       cout<<"El fichero no se abrió correctamente"<<endl;
+       }
+   }
+ }
