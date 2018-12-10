4e8ad8b1ac8ad4603b8eb601a1b73636ec75b7c1
Ángel Villegas del Río <angelvillegas7@gmail.com>
2018-12-10 19:28:13 +0100

    Añadir insertar alumno

---
+++ Practica4/Código/Grupo.cc
@@ -0,0 +1,21 @@
+#include "Grupo.h"
+#include "Alumno.h"
+
+using namespace std;
+
+bool Grupo:: AnadirIntegrante(string DNI, int NumGrupo){
+  ListaAlumnos Alumnos;
+    string linea;
+      ifstream entrada;
+      entrada.open("Alumnos.bin", ios::in| ios:binary);
+         while(getline(entrada,linea, ' ')){
+            if(Alumnos.DNI==DNI)&&(Alumnos.NumGrupo==NumGrupo;){
+                cout<<"El alumno ya existe"
+            return false;
+            }
+
+        }
+        else{
+            cout<<"El alumno se inserta en el grupo"<<endl;
+            return true;
+}
