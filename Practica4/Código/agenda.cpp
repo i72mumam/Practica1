d7c0e16b0c45287332cdc2a6c505f12da9ef822c
Ángel Villegas del Río <angelvillegas7@gmail.com>
2018-12-14 16:27:49 +0100

    Comentar las funciones

--- Practica4/Código/agenda.cpp
+++ Practica4/Código/agenda.cpp
@@ -1,13 +1,15 @@
 #include "Agenda.h"
 #include "Persona.h"
 
-void Agenda::MostrarAlumno(string DNI){ //Funciona
-string linea;
-  ifstream entrada;
-  entrada.open("Alumnos.bin", ios::in| ios::binary);
-    if(entrada.is_open()){
-      while(getline(entrada,linea, ' ')){
-        if(linea=DNI){
+
+//Mostrar Alumno
+void Agenda::MostrarAlumno(string DNI){
+string linea;       //Declaramos un string para leer los datos
+  ifstream entrada; //Declaramos el fichero
+  entrada.open("Alumnos.bin", ios::in| ios::binary); //Abrimos el fichero binario
+    if(entrada.is_open()){    //Comprobamos si el fichero está abierto
+      while(getline(entrada,linea, ' ')){  //Si el fichero se abre, leemos hasta el primer espacio y lo guardamos en la variable linea.
+        if(linea=DNI){                    //Si el DNI introducido corresponde con algún DNI en el fichero se muestra el alumno por pantalla
             cout<<linea<<' ';
         getline(entrada,linea, ' ');
           cout<<linea<<' ';
@@ -25,9 +27,9 @@ string linea;
           cout<<linea<<' ';
         }
       }
-    entrada.close();
+    entrada.close();       //Se cierra el fichero
     }
-    else{
+    else{             //Si no está abierto el fichero
     	cout<<" El fichero no se abrió correctamente"<<endl;
 	}
 }
@@ -51,12 +53,14 @@ string linea;
 return false;
 }*/
 
-void Agenda::MostrarTodosAlumnos(){     //Funciona
-  string linea;
-    ifstream entrada;
-    entrada.open("Alumnos.bin", ios::in| ios::binary);
-      if(entrada.is_open()){
-        while(getline(entrada,linea, ' ')){
+
+//Mostrar todos los alumnos
+void Agenda::MostrarTodosAlumnos(){
+  string linea;   //Declaramos un string para leer los datos
+    ifstream entrada;   //Declaramos el fichero
+    entrada.open("Alumnos.bin", ios::in| ios::binary);    //Abrimos el fichero binario
+      if(entrada.is_open()){    //Comprobación del fichero
+        while(getline(entrada,linea, ' ')){   //Mientras el fichero esté abierto se muestran por pantalla todos los alumnos
             cout<<linea<<' ';
           getline(entrada,linea, ' ');
             cout<<linea<<' ';
@@ -73,9 +77,9 @@ void Agenda::MostrarTodosAlumnos(){     //Funciona
           getline(entrada,linea, '\n');
             cout<<linea<<endl;
         }
-        entrada.close();
+        entrada.close();      //Se cierra el fichero
       }
-      else{
+      else{   //Si no está abierto el fichero
           cout << "El fichero no se abrió correctamente "<<endl;
       }
   }
